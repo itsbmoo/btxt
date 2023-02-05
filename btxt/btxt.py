37ddb64b5591a930f20d3af5ad4cd368a15bccb9
itsbmoo <96960171+itsbmoo@users.noreply.github.com>
2023-02-05 23:57:13 +0100

    btxt.py

---
+++ btxt/btxt.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import colorama
+from btxt.core.lexer import Lexer
+
+colorama.init(autoreset=True)
+
+
+def usage(error: str|None) -> str|None:
+    [print(f'{colorama.Fore.RED}ERROR: {error}') if error != None else None]
+    print('USAGE:\n\t* btxt <filename>     -     Render the file;')
+
+    
+if __name__ == '__main__':
+    os.system('cls')
+    match len(sys.argv):
+        case 2:
+            Lexer(sys.argv[1]).read_file()
+        case _:
+            usage('you have to provide an argument (filename)')
